d1e0f4f65c41ca1a706564eef0b46a43afaf0ae2
Thiago dos Santos Pereira <148633999+thiagopecli@users.noreply.github.com>
2025-12-11 00:57:48 -0300

    fix/adicionando url login

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login and Logout Settings
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'login'

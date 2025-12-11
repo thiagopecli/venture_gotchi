d571de28c3477b1650822aab59a5f40af9dff025
Thiago dos Santos Pereira <148633999+thiagopecli@users.noreply.github.com>
2025-12-11 00:59:42 -0300

    fix/criando url login e logout

--- core/urls.py
+++ core/urls.py
@@ -1,25 +1,13 @@
 from django.urls import path
 from . import views
-from views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
 
-    path('login/', LoginView.as_view(), name='login'),
-
-    # 1. Dashboard de Partidas
-    # Rota principal para listar partidas salvas e iniciar uma nova.
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'), 
-
-    # 2. Cadastro de Partidas
-    # Rota para exibir o formulário e processar a criação de uma nova simulação
     path('nova/', views.nova_partida, name='nova_partida'), 
-
-    # 3. Salvamento de Progresso
-    # Rota que recebe os dados do jogo e salva no BDR (Salvamento de Progresso)
-    # O <int:partida_id> é o ID da partida que o sistema deve atualizar.
     path('salvar/<int:partida_id>/', views.salvar_jogo, name='salvar_jogo'), 
-
-    # 4. Continuidade de Partidas
-    # Rota que busca os dados no BDR e restaura o estado do jogo (Carregamento
     path('carregar/<int:partida_id>/', views.carregar_jogo, name='carregar_jogo'),
 ]

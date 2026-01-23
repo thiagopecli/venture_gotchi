ad6971b32eb4d1ec31453e8b280119e15490e698
AnaC-Araujo <carolinaana887@gmail.com>
2026-01-22 22:43:33 -0300

    fix: melhorando permissoes entre estudante e aspirante

--- core/permissions.py
+++ core/permissions.py
@@ -16,7 +16,7 @@ def estudante_required(view_func):
         if not request.user.is_authenticated:
             return redirect('login')
         
-        if not request.user.is_estudante():
+        if not (request.user.is_estudante() or request.user.is_aspirante()):
             messages.error(
                 request, 
                 'Acesso negado. Esta funcionalidade está disponível apenas para Estudantes/Aspirantes.'

464393f427393e3f2c65ea4c13c02220c7831fcb
Thiago dos Santos Pereira <148633999+thiagopecli@users.noreply.github.com>
2026-01-23 18:49:25 -0300

    fix/corrigindo header corporativo

--- core/views.py
+++ core/views.py
@@ -370,11 +370,14 @@ def ranking(request):
     # 3. Filtro por categoria do usuário
     # Estudantes veem apenas startups de outros estudantes
     # Aspirantes veem apenas startups de outros aspirantes
+    # Profissionais Corporativos veem apenas startups de outros corporativos
     # Educadores veem todas as startups
     if request.user.categoria == User.Categorias.ESTUDANTE_UNIVERSITARIO:
         startups = startups.filter(partida__usuario__categoria=User.Categorias.ESTUDANTE_UNIVERSITARIO)
     elif request.user.categoria == User.Categorias.ASPIRANTE_EMPREENDEDOR:
         startups = startups.filter(partida__usuario__categoria=User.Categorias.ASPIRANTE_EMPREENDEDOR)
+    elif request.user.categoria == User.Categorias.PROFISSIONAL_CORPORATIVO:
+        startups = startups.filter(partida__usuario__categoria=User.Categorias.PROFISSIONAL_CORPORATIVO)
     
     # 4. Lógica de Filtro por Turma (Exclusivo Educador/Admin)
     # Verifica se é educador para permitir o filtro
